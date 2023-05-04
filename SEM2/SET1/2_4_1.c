5d13930e6d9a757a41f3d78e98c8aa639caf630f
Sudip ghosh <49285187+imsudip@users.noreply.github.com>
2023-05-04 14:53:13 +0530

    Update 2_4_1.c

--- SEM2/SET1/2_4_1.c
+++ SEM2/SET1/2_4_1.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int n, m;    // n = number of people, m = number of people to skip
-    int a[N][2]; // 2D array to store the people and their status (alive or executed)
+    int n, m;
+    int a[N][2];
     int i, j;
     printf("Enter the number of people: ");
     scanf("%d", &n);
@@ -12,29 +12,29 @@ int main()
     scanf("%d", &m);
     for (i = 1; i <= n; i++)
     {
-        a[i][0] = i; // initialize the people's numbers
-        a[i][1] = 1; // initialize the people's status as alive (1)
+        a[i][0] = i;
+        a[i][1] = 1;
     }
     i = 1;
     int alive = n;
     while (alive > 1)
-    { // repeat until only one person remains
+    {
         for (j = 1; j <= m; j++)
-        { // skip m people
+        {
             i++;
             if (i > n)
-                i = 1; // if reached the end of the circle, go back to the beginning
+                i = 2;
             if (a[i][1] == 0)
-                j--; // if the person is already executed, do not count them
+                j--;
         }
-        a[i][1] = 0; // execute the next person
+        a[i][1] = 0;
         alive--;
         i++;
     }
     for (i = 1; i <= N; i++)
     {
         if (a[i][1] == 1)
-        { // find the remaining person
+        {
             printf("The person to be freed is: %d\n", a[i][0]);
             break;
         }
